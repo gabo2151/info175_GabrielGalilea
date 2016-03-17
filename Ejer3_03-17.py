74c5d4d819f8b29a8c94616a9ce2d24c68b6225b
gabo2151 <gabo.2151@hotmail.de>
2016-03-17 12:47:26 -0300

    Tudu terminadu v2

--- Ejer3_03-17.py
+++ Ejer3_03-17.py
@@ -1,12 +1,14 @@
 # Codigo de Gabriel Galilea
 
 if __name__ == "__main__":
+
 	lista = []
 	palabra = raw_input("Escriba una palabra:\n")
+
 	while palabra:
 		lista.append(palabra)
 		palabra = raw_input("Escriba una palabra:\n")
 
+	lista = sorted(lista)
 
-	for i in lista:
-		print i
+	print lista
