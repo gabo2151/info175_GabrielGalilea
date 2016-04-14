83407527e14ba909d2c75e8c0193e63887233d62
gabo2151 <gabo.2151@hotmail.de>
2016-04-14 11:14:56 -0300

    correccion de nombres

--- 2016_04_14/fibonacci.py
+++ 2016_04_14/fibonacci.py
@@ -5,6 +5,6 @@ def fibboPulento(n):
 	elif n==1:
 		return 1
 	elif n>1:
-		return fibbo(n-1) + fibbo(n-2)
+		return fibboPulento(n-1) + fibboPulento(n-2)
 
 print fibboPulento( input("Ingrese el numero para calcular fibbonaci: ") )
