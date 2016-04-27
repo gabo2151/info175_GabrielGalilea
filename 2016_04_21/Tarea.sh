119d7c63ad90783de9874ce1d58c3ae54487e7c1
gabo2151 <gabo.2151@hotmail.de>
2016-04-27 13:57:28 -0300

    Sigue sin funcionar >:c

--- 2016_04_21/Tarea.sh
+++ 2016_04_21/Tarea.sh
@@ -7,8 +7,13 @@ echo "#    ZAFS - Zip A Folder Shell   #"
 echo "#                                #"
 echo "##################################"
 
+# Formato de fecha
+# date +"%Y_%m_%d" (YYYY_MM_DD)
+fecha_hoy=$(date +"%Y_%m_%d")
+#echo "$fecha_hoy"
+
 echo -n "# Carpeta a respaldar: "
 read res_fold
 echo -n "# Carpeta de destino: "
 read des_fold
-zip -r $des_fold/file_name.zip $res_fold
+zip -r $des_fold/"$fecha_hoy".zip $res_fold
