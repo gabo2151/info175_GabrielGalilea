88ebe94e7b6761e2ee5e7431fe02f018a781a308
gabo2151 <gabo.2151@hotmail.de>
2016-04-27 14:39:05 -0300

    Adornado... nada nuevo

--- 2016_04_21/Tarea.sh
+++ 2016_04_21/Tarea.sh
@@ -1,19 +1,46 @@
-#!/bin/bash
+#!/bin/sh
 #EL PROGRAMA! :D
-clear
-echo "##################################"
-echo "#                                #"
-echo "#    ZAFS - Zip A Folder Shell   #"
-echo "#                                #"
-echo "##################################"
 
 # Formato de fecha
 # date +"%Y_%m_%d" (YYYY_MM_DD)
 fecha_hoy=$(date +"%Y_%m_%d")
 #echo "$fecha_hoy"
 
-echo -n "# Carpeta a respaldar: "
-read res_fold
-echo -n "# Carpeta de destino: "
-read des_fold
-zip -r $des_fold/"$fecha_hoy".zip $res_fold
+menu(){
+	clear
+	echo "##################################"
+	echo "#                                #"
+	echo "#    ZAFS - Zip A Folder Shell   #"
+	echo "#                                #"
+	echo "##################################"
+	echo "#"
+	echo "#    Menu de opciones"
+	echo "#"
+	echo "#  1) Comprimir carpeta"
+	echo "#  0) Salir"
+	echo "#"
+	echo "#                      $(date +"%d/%m/%Y")"
+	echo "##################################"
+	echo -n "Elija una opcion: "
+	read num
+	case $num in
+		1) clear; execute_program;;
+		0) clear; exit;;
+		*) echo "La opcion $num no es valida"; sleep 3; clear; menu;
+	esac
+}
+
+execute_program(){
+	echo -n "Carpeta a respaldar: "
+	read res_fold
+	echo -n "Carpeta de destino: "
+	read des_fold
+	zip -r $des_fold/"$fecha_hoy".zip $res_fold
+	menu;
+}
+
+# Ejecuta menu
+while :
+do
+	menu
+done
