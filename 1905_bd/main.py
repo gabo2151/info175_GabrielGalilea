6fdafea807f0c979393c580c193558f8584e582e
gabo2151 <gabo.2151@hotmail.de>
2016-05-19 12:24:59 -0400

    primeras configuraciones

--- 1905_bd/main.py
+++ 1905_bd/main.py
@@ -4,17 +4,21 @@ import sqlite3 as lite
 import sys
  
 # TEST!!!!! 
-con = None
- 
-try:
-    con = lite.connect('test.db')
-    cur = con.cursor()    
-    cur.execute('SELECT SQLITE_VERSION()')
-    data = cur.fetchone()
-    print "SQLite version: %s" % data                
-except lite.Error, e:   
-    print "Error %s:" % e.args[0]
-    sys.exit(1)
-finally:    
-    if con:
-        con.close()
+#con = None
+# 
+#try:
+#    con = lite.connect('test.db')
+#    cur = con.cursor()    
+#    cur.execute('SELECT SQLITE_VERSION()')
+#    data = cur.fetchone()
+#    print "SQLite version: %s" % data                
+#except lite.Error, e:   
+#    print "Error %s:" % e.args[0]
+#    sys.exit(1)
+#finally:    
+#    if con:
+#        con.close()
+# END TEST!!!!!
+
+con = lite.connect('pos_empresa.db')
+
